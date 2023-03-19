a6f9e78abb351c8dff03bd923cc706970bd2fc9d
Gary <49135331+CheesyModz@users.noreply.github.com>
2023-03-19 14:10:39 -0400

    update

--- about_us.php
+++ about_us.php
@@ -17,14 +17,14 @@
 
   <div style="text-align:center;">
     <div class="header">
-      <h1 style="font-size:100px;color:lightcoral;">Appliances Canada</h1>
+      <h1 style="font-size:100px;color:lightcoral;">Smart Customer Services </h1>
       <h2>All appliances you need in one place.</h2>
     </div>
 
-    <h1>About Us</h2>
+    <h1 id="AboutUs">About Us</h2>
     <div class="text">
-      <p>Appliances Canada has a rich history over 40+ years of providing customers with high quality and long lasting appliances.
-        Founded in 1980, Appliances Canada is one of Canada's largest appliances store with over 10000 unique products, offering all kind of brands at a competitive pricing.
+      <p>Smart Customer Services has a rich history over 40+ years of providing customers with high quality and long lasting appliances.
+        Founded in 1980, Smart Customer Services is one of Canada's largest appliances store with over 10000 unique products, offering all kind of brands at a competitive pricing.
         Online since 2000, we have helped over 600,000 customers across Canada with their needs, with over 99% of our customers being satisfied.
         We have experienced buying appliances that are expensive and short-lived and are determined to do the opposite by provide last lasting appliances for everyone at an affordable price. 
         We deliver appliances to your door step within 1-3 days guaranteed when urgently needed and offer refunds within 60 days of purchase.
@@ -71,6 +71,18 @@
     </div>
     <button class="buttonContact" onclick="window.location.href='contact_us.php'">Contact Us</button>
 
+    <h1 id="FAQ">Frequently Asked Questions</h1>
+    <div class="text">
+      <h2>How long will my order take to arrive?</h2>
+      <p>Once we have received your order, we will arrive to deliver your order to your home. It will take 1-3 days for the deliver to arrive at your home.</p>
+      <h2>I need help with my appliances.</h2>
+      <p>Contact us <a style="color:blue;" href="contact_us.php">HERE</a></p>
+      <h2>What is the return policy?</h2>
+      <p>Orders can be cancelled within 48 hours of placement without any fees as long as order has not been shipped.
+        Orders can be returned within 60 days of purchaase. Damage returns are not accepted.
+      </p>
+    </div>
+    
     <h1>Customer Reviews</h2>
     <p>What our customers say about us.</p>
     <button class="buttonContact" onclick="window.location.href='reviews.php'">Reviews</button>
--- types_of_services.php
+++ types_of_services.php
@@ -3,6 +3,7 @@
 <meta charset="UTF-8">
 <title>Smart Customer Services</title>
 <link rel="stylesheet" href="./css/base.css">
+<link rel="stylesheet" href="./css/types_of_services.css">
 
 <style>
 </style>
@@ -13,8 +14,50 @@
     include('bar.php')
     ?>
 
+    <div class="text">
+        <h1 style="text-align:center;">Types of Services</h1>
+        <p>We provide the following services to our customers:</p>
+        <table>
+            <tbody>
+                <tr>
+                    <th>Orders</th>
+                    <td>
+                        <a href="">Payment Methods</a><br>
+                        <a href="">Cancelling Order</a>
+                    </td>
 
+                    <th>Delivery</th>
+                    <td>
+                        <a href="">Shipping Times</a><br>
+                        <a href="">Track an Order</a>
+                    </td>
+                </tr>
+                <tr>
+                    <th>About Us</th>
+                    <td>
+                        <a href="about_us.php#AboutUs">About Smart Customer Services</a><br>
+                        <a href="about_us.php#FAQ">FAQ</a><br>
+                        <a href="reviews.php">Customer Reviews</a>
+                    </td>
 
+                    <th>Contact Us</th>
+                    <td>
+                        <a href="contact_us.php">Contact Us Form</a><br>
+                        <a href="contact_us.php">Address, Phone, Email</a>
+                    </td>
+                </tr>
+                
+                <tr>
+                    <th>Privacy & Security</th>
+                    <td>
+                        <a href="">Privacy Policy</a><br>
+                        <a href="">Security Protection</a><br>
+                    </td>
+
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </body>
 
 </html>
